c95e07441cd1441661aec283d19f47fe67716f3a
Cosalpino <juillet.mikael@gmail.com>
2023-07-11 11:37:09 +0200

    Adding auto filling of stars with pc

--- Stars/main.js
+++ Stars/main.js
@@ -1,15 +1,9 @@
-var activeStars = document.getElementsByClassName('star--active');
 var percentage = document.getElementById("percentage");
-
-
 
 function starClicked(obj) {
 
-    if( obj.classList.contains("star--active")) obj.classList.remove("star--active")
-    else obj.classList.add("star--active")
+    var total =  obj * 100 / 5;
 
-    var total =  activeStars.length * 100 / 5;
+    document.getElementById("stars").style.setProperty("--pc",  total.toString() + "%");
     percentage.innerHTML = total.toString() + "%";
-
-    console.log(activeStars);
 }
